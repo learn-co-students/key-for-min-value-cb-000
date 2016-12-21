c544340429139f7cab4d682a10af19f428e35990
Kyle Greene <kgreene06@gmail.com>
2016-12-21 05:09:01 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash.empty?
+    return nil
+  end
+
+  min = 1000
+  final_name =" "
+  name_hash.each do |key, value|
+    if value < min
+      min = value 
+      final_name = key
+    end
+  end
+return final_name
+
 end
