1ddc2b9580a59c95a563184f9a7e4d5701480d34
dustinlikens <dustinlikens@icloud.com>
2017-01-12 13:13:38 -0800

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   return_key = nil
-  low = 10000000
+  min_value = nil
   name_hash.each do |key,value|
-    if value < low
-      low = value
+    if min_value == nil || value < min_value
+      min_value = value
       return_key = key
     end
   end
-  return return_key
+  return_key
 end
