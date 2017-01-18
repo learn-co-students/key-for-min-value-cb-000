05ddb99af292b2d8b6ddcfb459b5f37c101e902b
Andrew Schubert <andrew.schubert1986@gmail.com>
2017-01-18 17:10:25 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,19 @@
+=begin
 def key_for_min_value(name_hash)
   name_hash.collect do |name, number|
     return name if number < 500
   end
+end
+=end
+
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
 end
