ea46bb16933007ca317b7bfd03fd4ae77debff8f
Matthew <github email address>
2017-01-28 11:32:08 +0700

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,11 @@ def key_for_min_value(name_hash)
 		return nil
 	end
 	key_master = ""
-	smallest_value = 1000
+	test_value = 10000000
 	name_hash.each do |key, value|
 		
-		if(value < smallest_value)
-			smallest_value = value
+		if(value < test_value)
+			test_value = value
 			key_master = key
 		end
 	end
