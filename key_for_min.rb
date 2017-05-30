8753ed92ae2151f3e53eb52384a26d36abeeb449
Tony Nassif <tonynassif@gmail.com>
2017-05-30 00:44:14 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_number = 5000;
   key_value = ""
+  return nil  if name_hash == {}
   name_hash.each do |key, value|
     if value < lowest_number
       lowest_number = value
