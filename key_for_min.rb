901d911881e50b9adb7032b29abd7c7df4670d84
Matt Trussell <smtrussell@gmail.com>
2017-06-02 06:59:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,30 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.empty?
+    return nil
+  else
+    number_arr = []
+    name_hash.each do |key, value|
+      number_arr << value
+    end
+    lowest_number = number_arr[0]
+    number_arr.each do |number|
+      if number <= lowest_number
+        lowest_number = number
+      end
+    end
+    smallest_key = ""
+    name_hash.each do |key, value|
+      if value == lowest_number
+        smallest_key = key
+      end
+    end
+    smallest_key
+  end
 end
+
+
+# hash_pipeline = {one: 301, two: 56, three: 7, four: 9, five: 402}
+# hash_pipeline = {blake: 200, ashley: 2, adam: 1}
+# key_for_min_value({:blake => 200, :ashley =>2, :adam => 1})
