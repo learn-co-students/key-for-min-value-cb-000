e5d585b095c384c6bb89096d40bce13552261c10
Michael Anderson <michanders@msn.com>
2017-09-29 23:52:01 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.length == 0
+    return nil
+  else
+    mike = []
+    name_hash.collect do |key, value|
+      mike.push(value)
+    end
+    mike = mike.sort
+    name_hash.collect do |key, value|
+      if value == mike[0]
+        return key
+      end
+    end
+  end
 end
